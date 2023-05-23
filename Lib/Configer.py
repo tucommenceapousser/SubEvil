678292a14714027515ab56b68daa3527cb18f703
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2023-05-24 00:21:06 +0200

    Add files via upload

---
+++ Lib/Configer.py
@@ -0,0 +1,28 @@
+"""
+███████╗██╗   ██╗██████╗ ███████╗██╗   ██╗██╗██╗     
+██╔════╝██║   ██║██╔══██╗██╔════╝██║   ██║██║██║     
+███████╗██║   ██║██████╔╝█████╗  ██║   ██║██║██║     
+╚════██║██║   ██║██╔══██╗██╔══╝  ╚██╗ ██╔╝██║██║     
+███████║╚██████╔╝██████╔╝███████╗ ╚████╔╝ ██║███████╗
+╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝  ╚═══╝  ╚═╝╚══════╝
+#=====================================================
+#           Tool  Version : V 1.0
+#           Programmer    : EvilTwins
+#           Facebook      : @trhacknon
+#           Github        : @trhacknon
+#======================================================
+"""
+shodan_api_key = "AOs99PJzgI4gmMGyYebf41KiMZKTW1Sn"
+whoisxmlapi_api_key = "at_7KOrh0iROmxlPnZhXaV1eYY74AUca"
+certspotter_api_key = "k45725_X3dsfL0aGeqjLQyHjfJE"
+dnsdb_api_key = ""
+virustotal_api_key = "83d3ee27800e89e883983ac0ef5fd7fdeef1a4fcf8d1b8b437038f692c5a832e"
+recondev_api_key = ""
+passivetotal_api_key = "f13d76f1e062cd19"
+passivetotal_api_secret = "BH/WtZtJVEbNl3r3c+raFgKMaG046oZK"
+censys_api_id = "6876ab72-e65d-42fd-9c62-51cb20bcdc83"
+censys_api_secret = "oNu5CizhVOw5foJ7Yp02GMCOpLdOZwvr"
+facebook_access_token = ""
+binaryedge_api_key = "8d24c573-f6e0-4a6d-91d2-70db6ad9208d"
+spyse_api_key = ""
+securitytrails_API = "ePUGWNJqu7c5DA3NysHVZEsobAqkkYN2"
